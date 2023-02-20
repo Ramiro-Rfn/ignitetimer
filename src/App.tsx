8d36f525982c9a76c7659316f25509e224bf0eab
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2023-02-20 20:15:24 +0100

    setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/theme/default"
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        Ignite Timer projecto 02
-      </div>
+      <div>Ignite Timer projecto 02</div>
 
-      <GlobalStyle/>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
