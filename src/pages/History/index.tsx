194fdb97baae31c3eed808801f47abb680643627
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2023-02-21 16:00:03 +0100

    create History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,36 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export default function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {['Concluído', 'Em andamento', 'Interrompido'].map((value) => {
+              return (
+                <tr key={value}>
+                  <td>Estudar testes no react</td>
+                  <td>2 minutos</td>
+                  <td>Há 2 meses</td>
+                  <td>
+                    <Status statusColor="green">{value}</Status>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
